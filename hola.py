50df7d8e10e2c049da90725dbe0d17fbcf4cfd9b
Alexander Ivashkin <AlexanderIvashkin@Alexanders-MacBook-Pro.local>
2018-02-18 02:32:06 +0100

    Added listcomp version. Python is awesome

--- hola.py
+++ hola.py
@@ -5,15 +5,29 @@ def strips(midtime, stop):
     teststrips = []
     currentstrip = 0
     for strip in range(-3, 3):
-        currentstrip = midtime*((2**stop)**strip))
-        print(currentstrip)
+        currentstrip = midtime*((2**stop)**strip)
+        # print(currentstrip)
         teststrips.append(currentstrip)
     return teststrips
 
 print ("Hola Mundo!")
 print (strips.__doc__)
-mt = int(input("Gimme yo time! "))
+mt = float(input("Gimme yo time! "))
 st = float(input("Gimme yo stop-range! "))
 
-strips(mt, st)
-print("And now for something totally similar: " + strips(mt, st))
+# strips(mt, st)
+# print("And now for something totally similar: ")
+for s in strips(mt, st):
+    # print("Current strip: " + str(s))
+    print("Current strip: {}".format(s))
+
+def strips_listcomp(midtime, stop):
+    """Print test strip timings - listcomp version"""
+    return [midtime*((2**stop)**str) for str in range(-3,3)]
+
+print (strips_listcomp(mt, st))
+
+def strips_with_steps(midtime, stop):
+    """Print test strip timings - with steps between strips"""
+    return [midtime*((2**stop)**str) for str in range(-3,3)]
+
